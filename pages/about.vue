07141c06b0915474646c7cfed4d2c33e6835eb20
yanghq <yanghq@xfintech.com.cn>
2019-01-14 15:29:06 +0800

    * about module

--- pages/about.vue
+++ pages/about.vue
@@ -1,19 +1,26 @@
 <template>
-  <div 
-    id="posts" 
+  <div
+    id="posts"
     class="posts-expand">
-
-
-    <h3 id="关于"><a 
-      href="#关于" 
-      class="headerlink" 
-      title="关于"/>关于</h3><h4 id="88年-本科-烟台-在职"><a 
-        href="#88年-本科-烟台-在职" 
-        class="headerlink" 
-        title="88年/本科/烟台/在职"/>88年/本科/烟台/在职</h4><p>2011年开始自学前端，算入行较早的，可惜资质平平，没做出拿得出手的成绩，前端技术这几年发展突飞猛进，这是行业的机会，也是每一个走在前端路上的同行的机会，前端作为远离业务核心的一环，一直以来可取代性很高，我们应时刻保有危机感，即便现在前端热度颇高，但热度总会冷却，技术路上容不下投机的人，忽而看涨忽而看跌，只会把自己耽误了，前端的路还远没有到尽头，需要更强的自我驱动！</p>
-    <h3 id="技能"><a 
-      href="#技能" 
-      class="headerlink" 
+    <h3 id="学历"><a
+      href="#学历"
+      class="headerlink"
+      title="学历"/>学历</h3><h4 id="2013-2017 数学与计算机科学 深圳大学 本科"><a
+        href="#2013-2017 数学与计算机科学 深圳大学 本科"
+        class="headerlink"
+        title="2013-2017 数学与计算机科学 深圳大学 本科"/>2013-2017 数学与计算机科学 深圳大学 本科</h4>
+    <p>技术路上容不下投机的人，忽而看涨忽而看跌，只会把自己耽误了，虽然努力不一定有回报，但以未知面对未来却更令人畏惧！</p>
+    <h3 id="目的"><a
+      href="#目的"
+      class="headerlink"
+      title="目的"/>目的</h3><h4 id="初衷与展望"><a
+        href="#初衷与展望"
+        class="headerlink"
+        title="初衷与展望"/>初衷与展望</h4>
+    <p>写作是一种典型的知识构建活动，或者更准确的说，是一种对知识的重构活动；你应该（从现在开始就）写博客一文中也有两句很经典的话，一句是「书写是更好的思考」,另一句是「教是更好的学」。就我而言，工作与生活中，能接触到很多方方面面的知识，这有利于拓展知识的深度，但这方方面面的知识却也分割了知识图维，知识深度上难以加深，因此为了解决我现有的困境，也为了更好的分享知识成果，便诞生了该博客。</p>
+    <h3 id="技能"><a
+      href="#技能"
+      class="headerlink"
       title="技能"/>技能</h3><ul>
         <li>自主研发前端模块化UI框架，实现主题定制、JS模块缓存、前端组件化开发和前端自动化开发。</li>
         <li>30+各类前端插件，精通模块化开发，力挺约定大于配置。</li>
@@ -21,56 +28,56 @@
         <li>多个混合应用平台开发经验，包括phonegap/cordova、appcan、apicloud，自主研发混合应用开发框架，开发速度那叫一个快。</li>
         <li>熟悉前端自动化开发，自主研发基于Nodejs的自动化工具。</li>
       </ul>
-    <h3 id="经历"><a 
-      href="#经历" 
-      class="headerlink" 
-      title="经历"/>经历</h3><h4 id="2016-07-今-一家创业公司-前端架构师"><a 
-        href="#2016-07-今-一家创业公司-前端架构师" 
-        class="headerlink" 
+    <h3 id="经历"><a
+      href="#经历"
+      class="headerlink"
+      title="经历"/>经历</h3><h4 id="2016-07-今-一家创业公司-前端架构师"><a
+        href="#2016-07-今-一家创业公司-前端架构师"
+        class="headerlink"
         title="2016.07 - 今   一家创业公司 前端架构师"/>2016.07 - 今   一家创业公司 前端架构师</h4><p>负责前端团队技术建设，及公司所有产品线的交互设计和前端开发。</p>
-    <h4 id="2012-12-2016-05-山东捷瑞数字-技术经理"><a 
-      href="#2012-12-2016-05-山东捷瑞数字-技术经理" 
-      class="headerlink" 
+    <h4 id="2012-12-2016-05-山东捷瑞数字-技术经理"><a
+      href="#2012-12-2016-05-山东捷瑞数字-技术经理"
+      class="headerlink"
       title="2012.12 - 2016.05   山东捷瑞数字 技术经理"/>2012.12 - 2016.05   山东捷瑞数字 技术经理</h4><p>负责前端团队技术建设。<br>期间定制开发了内部开发框架；推出低成本高性能的响应式产品方案，成为公司网站类产品的标配；推出混合应用产品，极大缩短了APP类项目的开发周期；在职期间推进设计、前端、后端之间达成一致的交接规范，提高整个团队的交接效率和质量。</p>
-    <h4 id="2011-06-2012-11-潍坊盛邦广告-网络工程师"><a 
-      href="#2011-06-2012-11-潍坊盛邦广告-网络工程师" 
-      class="headerlink" 
+    <h4 id="2011-06-2012-11-潍坊盛邦广告-网络工程师"><a
+      href="#2011-06-2012-11-潍坊盛邦广告-网络工程师"
+      class="headerlink"
       title="2011.06 - 2012.11   潍坊盛邦广告 网络工程师"/>2011.06 - 2012.11   潍坊盛邦广告 网络工程师</h4><p>负责网站报价、资料准备、域名备案、服务器采购、前后端开发、部署上线、支持维护、SEO优化。</p>
-    <h3 id="联系"><a 
-      href="#联系" 
-      class="headerlink" 
+    <h3 id="联系"><a
+      href="#联系"
+      class="headerlink"
       title="联系"/>联系</h3><p>blog: <a href="https://refined-x.com">https://refined-x.com</a></p>
-    <p>github: <a 
-      href="https://github.com/tower1229" 
-      target="_blank" 
+    <p>github: <a
+      href="https://github.com/tower1229"
+      target="_blank"
       rel="external">https://github.com/tower1229</a></p>
-    <p>邮箱: <a 
-      href="mailto:tower1229@foxmail.com" 
-      target="_blank" 
+    <p>邮箱: <a
+      href="mailto:tower1229@foxmail.com"
+      target="_blank"
       rel="external">tower1229@foxmail.com</a></p>
-    <h4 id="QQ群：361917044"><a 
-      href="#QQ群：361917044" 
-      class="headerlink" 
+    <h4 id="QQ群：361917044"><a
+      href="#QQ群：361917044"
+      class="headerlink"
       title="QQ群：361917044"/>QQ群：361917044</h4><blockquote>
         <p>前端开发交流群，行业交流，工作心得，技术分享，加群需注明工作地、工作经验、作品。</p>
       </blockquote>
-    <h4 id="公众号：-看风景"><a 
-      href="#公众号：-看风景" 
-      class="headerlink" 
+    <h4 id="公众号：-看风景"><a
+      href="#公众号：-看风景"
+      class="headerlink"
       title="公众号： 看风景"/>公众号： 看风景</h4><blockquote>
         <p>程序员们的生活，面经、理财、投资、好物、羊毛、生活琐事，无所不包</p>
       </blockquote>
-    <p><img 
-      src="https://refined-x.com/asset/wechat.jpg" 
+    <p><img
+      src="https://refined-x.com/asset/wechat.jpg"
       alt="programmerslife"></p>
-    <h4 id="知识星球：前端路上"><a 
-      href="#知识星球：前端路上" 
-      class="headerlink" 
+    <h4 id="知识星球：前端路上"><a
+      href="#知识星球：前端路上"
+      class="headerlink"
       title="知识星球：前端路上"/>知识星球：前端路上</h4><blockquote>
         <p>付费咨询服务</p>
       </blockquote>
-    <p><img 
-      src="https://refined-x.com/asset/code-xiaomiquan.png" 
+    <p><img
+      src="https://refined-x.com/asset/code-xiaomiquan.png"
       alt="code-xiaomiquan"></p>
     <style>
       .lalala ul{overflow:hidden;padding:0;font-size:0}
@@ -79,26 +86,26 @@
       .lalala img{position: absolute; width: 100%;height:100%;}
     </style>
 
-    <div class="lalala"><br>    <ul><br>        <li><br>            <a 
-      href="https://www.aliyun.com/jianzhan/?userCode=y31qmczl" 
-      title="阿里云自营建站" 
-      target="_blank"><img 
-        src="/asset/ali-1.jpg" 
-        alt="阿里云自营建站"></a><br>        </li><br>        <li><br>            <a 
-          href="https://promotion.aliyun.com/ntms/act/qwbk.html?userCode=y31qmczl" 
-          title="ECS云服务器2折起" 
-          target="_blank"><img 
-            src="/asset/ali-2.jpg" 
-            alt="阿里云服务器"></a><br>        </li><br>        <li><br>            <a 
-              href="https://s.click.taobao.com/JPrrbZw" 
-              title="高性能" 
-              target="_blank"><img 
-                src="/asset/ali-3.jpg" 
-                alt="阿里云服务器"></a><br>        </li><br>        <li><br>            <a 
-                  href="https://s.click.taobao.com/U1srbZw" 
-                  title="学生专享9块9" 
-                  target="_blank"><img 
-                    src="/asset/ali-4.jpg" 
+    <div class="lalala"><br>    <ul><br>        <li><br>            <a
+      href="https://www.aliyun.com/jianzhan/?userCode=y31qmczl"
+      title="阿里云自营建站"
+      target="_blank"><img
+        src="/asset/ali-1.jpg"
+        alt="阿里云自营建站"></a><br>        </li><br>        <li><br>            <a
+          href="https://promotion.aliyun.com/ntms/act/qwbk.html?userCode=y31qmczl"
+          title="ECS云服务器2折起"
+          target="_blank"><img
+            src="/asset/ali-2.jpg"
+            alt="阿里云服务器"></a><br>        </li><br>        <li><br>            <a
+              href="https://s.click.taobao.com/JPrrbZw"
+              title="高性能"
+              target="_blank"><img
+                src="/asset/ali-3.jpg"
+                alt="阿里云服务器"></a><br>        </li><br>        <li><br>            <a
+                  href="https://s.click.taobao.com/U1srbZw"
+                  title="学生专享9块9"
+                  target="_blank"><img
+                    src="/asset/ali-4.jpg"
                     alt="阿里云服务器"></a><br>        </li><br>    </ul><br></div>
 
 
